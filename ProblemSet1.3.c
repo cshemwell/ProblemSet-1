b5b0df215b1a643310a4c30ef55ba1d9c2ed20f8
cshemwell <43276437+cshemwell@users.noreply.github.com>
2019-09-19 20:15:16 -0400

    Update ProblemSet1.3.c

--- ProblemSet1.3.c
+++ ProblemSet1.3.c
@@ -3,17 +3,15 @@
 
 int main (){
 
-
-      char *thing = "++++++++++";
+       char *thing = "++++++++++";
+      
       // tried %s/n, %d/n, %s "/
       // also tried creating/naming a data called ' ++++++++++ ' , but the +'s are making C mad.
       // error messages suggesting using %d/n instead, which got rid of one error, but not all of them
       // couldn't get past this first step, so I couldn't attempt making the line repeat 5 times
-       printf("%s/n", thing);
+       printf("%s /n", thing);
 
-
-       
-      return 0;
+      
       return 0;
 }
 
