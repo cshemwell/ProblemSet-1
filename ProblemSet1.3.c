d8ddcba874d1d136501461171cf569d4b7cd17d4
cshemwell <43276437+cshemwell@users.noreply.github.com>
2019-09-19 20:39:02 -0400

    Update ProblemSet1.3.c

--- ProblemSet1.3.c
+++ ProblemSet1.3.c
@@ -5,8 +5,11 @@ int main (){
 
        char *thing = "++++++++++";
     
-      // tried look
-       printf("%s %79s %79s %79s %79s\n", thing, thing, thing, thing, thing);
+       
+       //had to experiment with syntax. This is wasn't we learned, but it was the only way I could get it to work
+       //I didn't use the personalized data name or commas in the first section like we had, 
+       // and then followed by the personalized name.
+       printf("%s %79s %79s %79s %79s\n", thing);
 
       
       return 0;
