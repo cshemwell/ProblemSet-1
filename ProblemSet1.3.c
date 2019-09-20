67f73ea0534dcc6c068b437cb2403388909739c5
cshemwell <43276437+cshemwell@users.noreply.github.com>
2019-09-19 20:39:24 -0400

    Update ProblemSet1.3.c

--- ProblemSet1.3.c
+++ ProblemSet1.3.c
@@ -9,7 +9,7 @@ int main (){
        //had to experiment with syntax. This is wasn't we learned, but it was the only way I could get it to work
        //I didn't use the personalized data name or commas in the first section like we had, 
        // and then followed by the personalized name.
-       printf("%s %79s %79s %79s %79s\n", thing);
+       printf("%s %79s %79s %79s %79s\n", thing, thing, thing, thing, thing);
 
       
       return 0;
