d7a851576e9ca86392a762b3b4ab2dec70800d1e
cshemwell <43276437+cshemwell@users.noreply.github.com>
2019-09-19 20:32:41 -0400

    Update ProblemSet1.3.c

--- ProblemSet1.3.c
+++ ProblemSet1.3.c
@@ -6,7 +6,7 @@ int main (){
        char *thing = "++++++++++";
     
       // tried look
-       printf("thing: %s, thing: %20s, thing: %20s, thing: %20s, thing: %20s\n", thing, thing, thing, thing, thing);
+       printf("thing: %s, %20s, %20s, %20s, %20s\n", thing, thing, thing, thing, thing);
 
       
       return 0;
