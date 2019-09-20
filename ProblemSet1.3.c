43cf5f4a4ec93b708fc7a8dabe49af917259f96b
cshemwell <43276437+cshemwell@users.noreply.github.com>
2019-09-19 20:11:14 -0400

    Update ProblemSet1.3.c

--- ProblemSet1.3.c
+++ ProblemSet1.3.c
@@ -9,7 +9,7 @@ int main (){
       // also tried creating/naming a data called ' ++++++++++ ' , but the +'s are making C mad.
       // error messages suggesting using %d/n instead, which got rid of one error, but not all of them
       // couldn't get past this first step, so I couldn't attempt making the line repeat 5 times
-       printf("%s", thing/n, thing/n, thing/n, thing/n, thing/n);
+       printf("%s", thing, thing, thing, thing, thing);
 
 
        
